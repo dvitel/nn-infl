5d7eccdb93d2e735ac86095d0c0827c7d171d908
dvitel <viteldmitry@gmail.com>
2025-03-16 00:50:30 -0400

    small fix

--- slurm/4-finetune2-cos-hpg.sh
+++ slurm/4-finetune2-cos-hpg.sh
@@ -22,27 +22,27 @@ method_name=cos
 # WE layer
 for run_id in {0..4}; do
     echo "Starting finetune2 WE $task $run_id"
-    INFL_SEED=$run_id INFL_CWD=$task_cwd python home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.word_embeddings\\..\* --tag='WE'
+    INFL_SEED=$run_id INFL_CWD=$task_cwd python /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.word_embeddings\\..\* --tag='WE'
     echo "Done finetune2 WE $task $run_id"
 done
 
 # First 16 layers of LORA
 for run_id in {0..4}; do
     echo "Starting finetune2 LORA 0-15 $task $run_id"
-    INFL_SEED=$run_id INFL_CWD=$task_cwd python home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.layer\\.\(\[0-9\]\|1\[0-5\]\)\\..\*\\.lora_\(A\|B\)\\..\* --tag='0-15'
+    INFL_SEED=$run_id INFL_CWD=$task_cwd python /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.layer\\.\(\[0-9\]\|1\[0-5\]\)\\..\*\\.lora_\(A\|B\)\\..\* --tag='0-15'
     echo "Done finetune2 LORA 0-15 $task $run_id"
 done
 
 # Last 8 layers of LORA
 for run_id in {0..4}; do
     echo "Starting finetune2 LORA 16-23 $task $run_id"
-    INFL_SEED=$run_id INFL_CWD=$task_cwd python home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.layer\\.\(1\[6-9\]\|2\[0-3\]\)\\..\*\\.lora_\(A\|B\)\\..\* --tag='16-23'
+    INFL_SEED=$run_id INFL_CWD=$task_cwd python /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.layer\\.\(1\[6-9\]\|2\[0-3\]\)\\..\*\\.lora_\(A\|B\)\\..\* --tag='16-23'
     echo "Done finetune2 LORA 16-23 $task $run_id"
 done
 
 # Classifier
 for run_id in {0..4}; do
     echo "Starting finetune2 CL $task $run_id"
-    INFL_SEED=$run_id INFL_CWD=$task_cwd python home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.classifier\\..\* --tag='CL'
+    INFL_SEED=$run_id INFL_CWD=$task_cwd python /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task --filter-method=infl --infl-method=$method_name --i-prefix=i_b --unfreeze-regex=.\*\\.word_embeddings\\..\* --module-pattern=.\*\\.classifier\\..\* --tag='CL'
     echo "Done finetune2 CL $task $run_id"
 done
