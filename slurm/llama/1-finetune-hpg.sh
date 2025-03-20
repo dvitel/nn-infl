afc5231db3c687ddbe130bd376c449e9e7dc1824
dvitel <viteldmitry@gmail.com>
2025-03-20 01:02:44 -0400

    tune llama

---
+++ slurm/llama/1-finetune-hpg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --job-name=llama-tune
+#SBATCH --time=72:00:00
+#SBATCH --output llama-tune-%j.out
+#SBATCH -D /blue/anshumanc.usf/nn-infl/llama
+#SBATCH -p hpg-ai # run on partition general
+#SBATCH --open-mode=append
+#SBATCH --gpus=1 # 1 GPU
+#SBATCH --array=0-3
+
+module load conda/24.7.1
+conda activate /home/dvitel.usf/torch-env
+
+tasks=("qnli" "mrpc" "sst2" "qqp")
+
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+
+task_cwd=/blue/anshumanc.usf/nn-infl/llama/$task
+
+for run_id in {0..4}; do
+    echo "Starting finetuning $task $run_id"
+    HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
+        /home/dvitel.usf/nn-infl/src/exp.py finetune --task=$task --model=meta-llama/Llama-3.2-1B \
+                 --num-epochs=15 --unfreeze-regex=.\*\\.embed_tokens\\..\* --lora-targets=v_proj
+    echo "Done finetuning $task $run_id"
+done
