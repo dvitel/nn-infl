1a2fd2f412b21979b387d388385e8bc26f137077
dvitel <viteldmitry@gmail.com>
2025-03-21 03:20:03 -0400

    roberta fix

---
+++ slurm/roberta/0-preprocess-hpg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#SBATCH --job-name=glue-r-ds
+#SBATCH --time=72:00:00
+#SBATCH --output glue-r-ds-%j.out
+#SBATCH -D /blue/anshumanc.usf/nn-infl/roberta
+#SBATCH -p hpg-ai # run on partition general
+#SBATCH --open-mode=append
+#SBATCH --gpus=1 # 1 GPU
+#SBATCH --array=0-3
+
+module load conda/24.7.1
+conda activate /home/dvitel.usf/torch-env
+
+# one job per task 
+tasks=("qnli" "mrpc" "sst2" "qqp")
+
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+
+task_cwd=/blue/anshumanc.usf/nn-infl/roberta/$task
+
+mkdir -p $task_cwd
+
+for run_id in {0..4}; do
+    echo "Starting preprocess $task $run_id"
+    INFL_SEED=$run_id INFL_CWD=$task_cwd python \
+        /home/dvitel.usf/nn-infl/src/exp.py preprocess --task=$task --tokenizer-name=roberta-large
+    echo "Done preprocess $task $run_id"
+done
