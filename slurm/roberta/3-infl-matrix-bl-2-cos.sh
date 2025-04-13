e246511385fddba73c35b330b3c430947974b1ad
dvitel <viteldmitry@gmail.com>
2025-04-12 23:16:38 -0400

    infl on new datasets

---
+++ slurm/roberta/3-infl-matrix-bl-2-cos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --job-name=r-i-cos2
+#SBATCH --time=72:00:00
+#SBATCH --output i-cos2-%a.out
+#SBATCH -D /blue/anshumanc.usf/nn-infl/roberta
+#SBATCH -p hpg-ai
+#SBATCH --gpus=1 # 1 GPU
+#SBATCH --mem=4G
+#SBATCH --array=0-4
+
+module load conda/24.7.1
+conda activate /home/dvitel.usf/torch-env
+
+tasks=("cola" "mnli" "rte" "wnli" "stsb")
+
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+
+task_cwd=/blue/anshumanc.usf/nn-infl/roberta/$task
+
+method_name=cos
+mem_koef=1.1
+
+for run_id in {0..4}; do
+
+    echo "Infl matrix $task $run_id $method_name"
+    HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
+        /home/dvitel.usf/nn-infl/src/exp.py infl-matrix --task=$task --methods=$method_name --mem-koef=$mem_koef --m-prefix=m_bl --i-prefix=i_bl
+    echo "----- Done $task $run_id $method_name"
+
+done
---
+++ slurm/roberta/3-infl-matrix-bl-2-hf-we-topk.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --job-name=r-hf-we-t2
+#SBATCH --time=72:00:00
+#SBATCH --output i-hf-we-t2-%a.out
+#SBATCH -D /blue/anshumanc.usf/nn-infl/roberta
+#SBATCH -p hpg-ai
+#SBATCH --gpus=1 # 1 GPU
+#SBATCH --mem=8G # default 4GB
+#SBATCH --array=0-4
+
+module load conda/24.7.1
+conda activate /home/dvitel.usf/torch-env
+
+tasks=("cola" "mnli" "rte" "wnli" "stsb")
+
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+
+task_cwd=/blue/anshumanc.usf/nn-infl/roberta/$task
+
+method_name='hf_we_topk_10'
+mem_koef=2.2
+
+for run_id in {0..4}; do
+
+    echo "Infl matrix $task $run_id $method_name"
+    HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
+        /home/dvitel.usf/nn-infl/src/exp.py infl-matrix --task=$task --methods=$method_name --mem-koef=$mem_koef --m-prefix=m_bl --i-prefix=i_bl
+    echo "----- Done $task $run_id $method_name"
+done
