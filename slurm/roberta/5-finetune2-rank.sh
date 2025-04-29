4d55d9ec1e5db5afda5cc57ee0addc153a4e7173
dvitel <viteldmitry@gmail.com>
2025-04-29 16:23:49 -0400

    roberta rank and vote

--- slurm/roberta/5-finetune2-rank.sh
+++ slurm/roberta/5-finetune2-rank.sh
@@ -6,7 +6,7 @@
 #SBATCH -p hpg-ai
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH --mem=16G # default 4GB
-#SBATCH --array=0-7
+#SBATCH --array=0-7%6
 
 module load conda/24.7.1
 conda activate /home/dvitel.usf/torch-env
@@ -17,19 +17,19 @@ task=${tasks[$SLURM_ARRAY_TASK_ID]}
 
 task_cwd=/blue/anshumanc.usf/nn-infl/roberta/$task
 
-agg_method='mean'
+agg_method='rank-c'
 method_names=('hf' 'datainf' 'cos')
 # module_names=('WE' '' '00-05' '06-11' '12-17' '18-23' '00-05 A' '06-11 A' '12-17 A' '18-23 A' '00-05 B' '06-11 B' '12-17 B' '18-23 B' '23' 'CL')
 module_names=('WE' '' '00-05' '06-11' '12-17' '18-23' 'CL')
 
 for method_name in "${method_names[@]}"; do
     for module_name in "${module_names[@]}"; do
-        for run_id in {0..4}; do
+        for run_id in {5..9}; do
             echo "Finetune2 $task $run_id $method_name $agg_method $module_name"
             HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
                 /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task \
                 --infl-method=$method_name --agg-method=$agg_method --module-name=$module_name \
-                --s-prefix=s_bl --metrics-file=$task-bl.jsonlist --filter-perc=0.1
+                --s-prefix=s_bl --metrics-file=$task-rank.jsonlist --filter-perc=0.3
             echo "Done finetune2 $task $run_id $method_name $agg_method $module_name"
             echo "----------------------------------"
         done
@@ -40,12 +40,12 @@ new_method_names=('hf_we_' 'hf_we_topk_10')
 module_name='WE'
 
 for method_name in "${new_method_names[@]}"; do
-    for run_id in {0..4}; do
+    for run_id in {5..9}; do
         echo "Finetune2 $task $run_id $method_name $agg_method $module_name"
         HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
             /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task \
             --infl-method=$method_name --agg-method=$agg_method --module-name=$module_name \
-            --s-prefix=s_bl --metrics-file=$task-bl.jsonlist --filter-perc=0.1
+            --s-prefix=s_bl --metrics-file=$task-rank.jsonlist --filter-perc=0.3
         echo "Done finetune2 $task $run_id $method_name $agg_method $module_name"
         echo "----------------------------------"
     done
@@ -56,12 +56,12 @@ agg_method=''
 module_name=''
 
 for method_name in "${base_method_names[@]}"; do
-    for run_id in {0..4}; do
+    for run_id in {5..9}; do
         echo "Finetune2 $task $run_id $method_name $agg_method $module_name"
         HF_TOKEN=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 INFL_SEED=$run_id INFL_CWD=$task_cwd python \
             /home/dvitel.usf/nn-infl/src/exp.py finetune2 --task=$task \
             --infl-method=$method_name --agg-method=$agg_method --module-name=$module_name \
-            --s-prefix=s_bl --metrics-file=$task-bl.jsonlist --filter-perc=0.1
+            --s-prefix=s_bl --metrics-file=$task-rank.jsonlist --filter-perc=0.3
         echo "Done finetune2 $task $run_id $method_name $agg_method $module_name"
         echo "----------------------------------"
     done
