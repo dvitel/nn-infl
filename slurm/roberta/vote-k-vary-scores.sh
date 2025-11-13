4069e671ba771dbb8be0742f29a215c6e7ed347e
dvitel <viteldmitry@gmail.com>
2025-11-13 13:18:47 -0500

    small fix

--- slurm/roberta/vote-k-vary-scores.sh
+++ slurm/roberta/vote-k-vary-scores.sh
@@ -23,7 +23,7 @@ for run_id in {0..9}; do
         /home/dvitel.usf/nn-infl/src/exp.py scores --task=$task \
         --infl-methods=datainf,cos,hf,hf_we_,hf_we_topk_10,rand,denoise \
         --agg-methods=vote2-c-10,vote2-c-20,vote2-c-30,vote2-c-40,vote2-c-50,vote2-c-60,vote2-c-70,vote2-c-80,vote2-c-90,vote2-c-100 \
-        --m-prefix=m_bl --i-prefix=i_bl --s-prefix=s_bl \
+        --m-prefix=m_bl --i-prefix=i_bl --s-prefix=s_vote_k \
         --group-file=../groups.json
 
     echo "Done scores $task $run_id"
