89877592678a4c067529c540037d611c29d1476e
dvitel <viteldmitry@gmail.com>
2025-11-14 00:53:13 -0500

    simplify

--- slurm/mistral/ds.sh
+++ slurm/mistral/ds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=m-ds-mr
+#SBATCH --job-name=m-ds
 #SBATCH --time=72:00:00
-#SBATCH --output ds-mr-%a.out
+#SBATCH --output ds-%a.out
 #SBATCH -D /blue/anshumanc.usf/nn-infl/mistral
 #SBATCH -p hpg-b200
 #SBATCH --gpus=1
 #SBATCH --mem=16G
-#SBATCH --array=0-0
+#SBATCH --array=0-2
 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 nvidia-smi
@@ -16,7 +16,11 @@ module load conda
 conda activate /blue/anshumanc.usf/nn-infl/nn-infl-env
 
 echo 'Running SFT...'
-task=mathR
+tasks=("sentense" "math" "mathR")
+datasets=("grammars_train" "math_without_reason_train" "math_with_reason_train")
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
+
 task_cwd=/blue/anshumanc.usf/nn-infl/mistral/$task
 
 HF_HOME=/blue/anshumanc.usf/nn-infl/.cache \
@@ -24,10 +28,10 @@ HF_TOKEN=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 \
 INFL_SEED=0 \
 python /home/dvitel.usf/nn-infl/src/sft_trainer.py \
     --model-name mistralai/Mistral-7B-v0.3 \
-    --dataset-name /home/dvitel.usf/nn-infl/datasets/math_with_reason_train.hf \
+    --dataset-name /home/dvitel.usf/nn-infl/datasets/$dataset.hf \
     --output-dir $task_cwd/checkpoint \
     --dataset-text-field text \
-    --learning-rate 3e-4 \
+    --learning-rate 1e-3 \
     --batch-size 128 \
     --num-train-epochs 10
 
--- slurm/qwen/ds.sh
+++ slurm/qwen/ds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=q-ds-m
+#SBATCH --job-name=q-ds
 #SBATCH --time=72:00:00
-#SBATCH --output ds-m-%a.out
+#SBATCH --output ds-%a.out
 #SBATCH -D /blue/anshumanc.usf/nn-infl/qwen
 #SBATCH -p hpg-b200
 #SBATCH --gpus=1
 #SBATCH --mem=16G
-#SBATCH --array=0-0
+#SBATCH --array=0-2
 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 nvidia-smi
@@ -16,7 +16,11 @@ module load conda
 conda activate /blue/anshumanc.usf/nn-infl/nn-infl-env
 
 echo 'Running SFT...'
-task=math
+tasks=("sentense" "math" "mathR")
+datasets=("grammars_train" "math_without_reason_train" "math_with_reason_train")
+task=${tasks[$SLURM_ARRAY_TASK_ID]}
+dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
+
 task_cwd=/blue/anshumanc.usf/nn-infl/qwen/$task
 
 HF_HOME=/blue/anshumanc.usf/nn-infl/.cache \
@@ -24,7 +28,7 @@ HF_TOKEN=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 \
 INFL_SEED=0 \
 python /home/dvitel.usf/nn-infl/src/sft_trainer.py \
     --model-name Qwen/Qwen2.5-1.5B \
-    --dataset-name /home/dvitel.usf/nn-infl/datasets/math_without_reason_train.hf \
+    --dataset-name /home/dvitel.usf/nn-infl/datasets/$dataset.hf \
     --output-dir $task_cwd/checkpoint \
     --dataset-text-field text \
     --learning-rate 1e-3 \
